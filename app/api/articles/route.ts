368b9e7e12e63f7c323a0619077c80e1dd94d041
Seonmi Jung <devkatej927@gmail.com>
2023-11-01 23:30:01 +0900

    feat : made POST method

--- app/api/articles/route.ts
+++ app/api/articles/route.ts
@@ -1,1 +1,10 @@
+import connectMongoDB from '@/libs/mongodb'
+import Article from '@/models/topic'
+import { NextResponse } from 'next/server'
 
+export async function POST(request: Request) {
+  const { title, content } = await request.json()
+  await connectMongoDB()
+  await Article.create({ title, content })
+  return NextResponse.json({ message: 'Article created' }, { status: 201 })
+}
