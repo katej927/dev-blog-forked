909a0e6f4abe06e37ce277eaa31c11b8da4a7c73
Seonmi Jung <devkatej927@gmail.com>
2023-11-03 14:16:56 +0900

    feat : made getArticleById method - to get article by id, i made an logic and applied it

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,3 +1,22 @@
-export default function ArticlePage() {
+import { Params } from 'next/dist/shared/lib/router/utils/route-matcher'
+
+const getArticleById = async (id: string) => {
+  try {
+    const res = await fetch(`http://localhost:3000/api/articles/${id}`, { cache: 'no-store' })
+
+    if (!res.ok) {
+      throw new Error('Failed to fetch an article.')
+    }
+
+    return res.json()
+  } catch (error) {
+    console.log('error')
+  }
+}
+
+export default async function ArticlePage({ params }: Params) {
+  const { id } = params
+  const data = await getArticleById(id)
+
   return <div>ArticlePage</div>
 }
