cb0ff7ab454544e420bc5de30a531e229647aec5
Seonmi Jung <devkatej927@gmail.com>
2023-11-10 21:52:51 +0900

    feat : add modules & format

---
+++ src/containers/Home/Article/Editor/_shared/index.ts
@@ -0,0 +1,1 @@
+export { modules, formats } from './constants'
--- src/containers/Home/Article/Editor/index.tsx
+++ src/containers/Home/Article/Editor/index.tsx
@@ -4,16 +4,26 @@ import { useState } from 'react'
 import ReactQuill from 'react-quill'
 import 'react-quill/dist/quill.snow.css'
 
+import { formats, modules } from './_shared'
+
 const Editor = () => {
   const [content, setContent] = useState('')
 
   return (
-    <div>
+    <div style={{ display: 'flex' }}>
       <ReactQuill
         theme="snow"
         style={{ height: '600px' }}
         value={content}
         onChange={setContent}
+        modules={modules}
+        formats={formats}
+      />
+      <ReactQuill
+        theme="bubble"
+        readOnly
+        value={content}
+        style={{ width: '50%', height: '600px' }}
       />
     </div>
   )
