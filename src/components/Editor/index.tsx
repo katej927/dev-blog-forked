9f2b900e43caff3bf0feac8728228ad22f1fd338
Seonmi Jung <devkatej927@gmail.com>
2023-11-11 16:46:01 +0900

    refactor : to fix CORS error, revised many things - add useMemo - add 'throw new Error' logic to avoid error - fixed syntax errors in imageHandler - add headers in firebase.json - installed cors

--- src/components/Editor/index.tsx
+++ src/components/Editor/index.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useRef, useState } from 'react'
+import { useMemo, useRef, useState } from 'react'
 import ReactQuill from 'react-quill'
 import 'react-quill/dist/quill.snow.css'
 // eslint-disable-next-line import/no-extraneous-dependencies
@@ -22,28 +22,38 @@ const Editor = () => {
     input.click()
 
     input.addEventListener('change', async () => {
+      if (!quillRef.current || !input.files) {
+        throw new Error('quillRef.current or input.files has no value')
+      }
+
       const editor = quillRef.current.getEditor()
       const file = input.files[0]
       const range = editor.getSelection(true)
       try {
         // 파일명을 "image/Date.now()"로 저장
         const storageRef = ref(storage, `image/${Date.now()}`)
-        // Firebase Method : uploadBytes, getDownloadURL
-        await uploadBytes(storageRef, file).then((snapshot) => {
-          getDownloadURL(snapshot.ref).then((url) => {
-            // 이미지 URL 에디터에 삽입
-            editor.insertEmbed(range.index, 'image', url)
-            // URL 삽입 후 커서를 이미지 뒷 칸으로 이동
-            editor.setSelection(range.index + 1)
-          })
-        })
+        // // Firebase Method : uploadBytes, getDownloadURL
+        // await uploadBytes(storageRef, file).then((snapshot) => {
+        //   getDownloadURL(snapshot.ref).then((url) => {
+        //     // 이미지 URL 에디터에 삽입
+        //     editor.insertEmbed(range.index, 'image', url)
+        //     // URL 삽입 후 커서를 이미지 뒷 칸으로 이동
+        //     editor.setSelection(range.index + 1)
+        //   })
+        // })
+
+        const snapshot = await uploadBytes(storageRef, file)
+        const url = await getDownloadURL(snapshot.ref)
+
+        // 이미지를 현재 커서 위치에 삽입
+        editor.insertEmbed(editor.getSelection()?.index || 0, 'image', url)
       } catch (error) {
         console.log(error)
       }
     })
   }
 
-  const modules = convertModules(imageHandler)
+  const modules = useMemo(() => convertModules(imageHandler), [])
 
   return (
     <>
