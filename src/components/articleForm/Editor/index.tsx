221965fe3b06284856dca889637c8d8a16dea48b
Seonmi Jung <devkatej927@gmail.com>
2023-11-12 19:36:21 +0900

    remove : removeEventHandler for next PR - reason : can't resolve in this PR

--- src/components/articleForm/Editor/index.tsx
+++ src/components/articleForm/Editor/index.tsx
@@ -18,45 +18,7 @@ interface Props {
 const Editor = ({ contentHtml, onChangeContent }: Props) => {
   const quillRef = useRef<ReactQuill>()
 
-  const imageHandler = () => {
-    const input = document.createElement('input')
-
-    input.setAttribute('type', 'file')
-    input.setAttribute('accept', 'image/*')
-    input.click()
-
-    input.addEventListener('change', async () => {
-      if (!quillRef.current || !input.files) {
-        throw new Error('quillRef.current or input.files has no value')
-      }
-
-      const editor = quillRef.current.getEditor()
-      const file = input.files[0]
-      const range = editor.getSelection(true)
-      try {
-        // TODO: 이거 정리하기
-        // 파일명을 "image/Date.now()"로 저장
-        const storageRef = ref(storage, `image/${Date.now()}`)
-        // // Firebase Method : uploadBytes, getDownloadURL
-        // await uploadBytes(storageRef, file).then((snapshot) => {
-        //   getDownloadURL(snapshot.ref).then((url) => {
-        //     // 이미지 URL 에디터에 삽입
-        //     editor.insertEmbed(range.index, 'image', url)
-        //     // URL 삽입 후 커서를 이미지 뒷 칸으로 이동
-        //     editor.setSelection(range.index + 1)
-        //   })
-        // })
-
-        const snapshot = await uploadBytes(storageRef, file)
-        const url = await getDownloadURL(snapshot.ref)
-
-        // 이미지를 현재 커서 위치에 삽입
-        editor.insertEmbed(editor.getSelection()?.index || 0, 'image', url)
-      } catch (error) {
-        console.log(error)
-      }
-    })
-  }
+  const imageHandler = () => {}
 
   const modules = useMemo(() => convertModules(imageHandler), [])
 
