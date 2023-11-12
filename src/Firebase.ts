81bfd76f94a27aa71fc3785ff62626cf7232383a
Seonmi Jung <devkatej927@gmail.com>
2023-11-12 20:10:54 +0900

    refactor : revised firebaseConfig's values - hide into local env file

--- src/Firebase.ts
+++ src/Firebase.ts
@@ -7,15 +7,13 @@ import { getStorage } from 'firebase/storage'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyAEh4CVuS3Xuc9D_Av5L__X0Lz0OMROkL8',
-  authDomain:
-    '[dev-blog-b5961.firebaseapp.com](http://dev-blog-b5961.firebaseapp.com/)',
-  projectId: 'dev-blog-b5961',
-  storageBucket:
-    '[dev-blog-b5961.appspot.com](http://dev-blog-b5961.appspot.com/)',
-  messagingSenderId: '228541307901',
-  appId: '1:228541307901:web:6313c35e00a4d183a6d794',
-  measurementId: 'G-3CG73201YY',
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 }
 
 // Initialize Firebase
