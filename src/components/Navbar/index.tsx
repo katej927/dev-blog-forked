a219bf1cc00290738d06e0cddc5429dfe2eabcc4
Seonmi Jung <devkatej927@gmail.com>
2023-11-28 20:13:43 +0900

    refactor : moved Search folder's location - reason : it doesn't need every where for now. and it was interrupt to route in each article page when github login

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,12 +1,9 @@
 import Link from 'next/link'
 
-import Search from './Search'
-
 const Navbar = () => {
   return (
     <nav style={{ display: 'flex' }}>
       <Link href={'/'}>Home</Link>
-      <Search />
       <Link href={'/article/write'}>Write</Link>
     </nav>
   )
