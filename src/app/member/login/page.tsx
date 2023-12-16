9aa50deeb8321dd39733f5c38c0644d816caca1f
Seonmi Jung <devkatej927@gmail.com>
2023-12-16 14:14:42 +0900

    feat : made LoginForm and applied it

--- src/app/member/login/page.tsx
+++ src/app/member/login/page.tsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import LoginForm from '@/components/LoginForm'
 
 const LoginPage = () => {
-  return <div>LoginPage</div>
+  return (
+    <div>
+      <LoginForm />
+    </div>
+  )
 }
 
 export default LoginPage
--- src/components/LoginForm/index.tsx
+++ src/components/LoginForm/index.tsx
@@ -1,5 +1,24 @@
+import Link from 'next/link'
+
 const LoginForm = () => {
-  return <div>LoginForm</div>
+  return (
+    <div>
+      <div>
+        <h1>Enter the details</h1>
+
+        <form>
+          <input type="text" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <button>Login</button>
+
+          <div style={{ color: 'red' }}>Error message</div>
+          <Link href={'/register'}>
+            Don&apos;t have an account? <span>Register</span>
+          </Link>
+        </form>
+      </div>
+    </div>
+  )
 }
 
 export default LoginForm
