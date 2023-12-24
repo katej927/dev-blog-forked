194a23cc544d43e68356a6b12bb1bcab40697269
Seonmi Jung <devkatej927@gmail.com>
2023-12-24 14:09:46 +0900

    feat : add POST in register of authentication

---
+++ src/apis/authentication/register/route.ts
@@ -0,0 +1,16 @@
+import { NextResponse } from 'next/server'
+
+export async function POST(req) {
+  try {
+    const { name, email, password } = await req.json()
+
+    return NextResponse.json({ message: 'User registered.' }, { status: 201 })
+  } catch (error) {
+    return NextResponse.json(
+      {
+        message: 'An error ocurred while registering the user.',
+      },
+      { status: 500 },
+    )
+  }
+}
