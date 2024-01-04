a6ca2b7237f936ba3254a75213e09c7c09adf7d7
Seonmi Jung <devkatej927@gmail.com>
2024-01-04 12:44:24 +0900

    ix : fixed setSelection's type error, refactored annotation - annotation reason : to get explanation by hovering

--- src/components/ArticleForm/Editor/index.tsx
+++ src/components/ArticleForm/Editor/index.tsx
@@ -31,15 +31,24 @@ const Editor = ({ contentHtml, onChangeContent }: Props) => {
       const range = editor.getSelection(true)
 
       try {
-        // 파일명을 "image/Date.now()"로 저장
+        /**
+         * 파일명을 "image/Date.now()"로 저장
+         */
         const storageRef = ref(storage, `image/${Date.now()}`)
-        // Firebase Method : uploadBytes, getDownloadURL
+
+        /**
+         * Firebase Method : uploadBytes, getDownloadURL
+         */
         await uploadBytes(storageRef, file).then((snapshot) => {
           getDownloadURL(snapshot.ref).then((url) => {
-            // 이미지 URL 에디터에 삽입
+            /**
+             * 이미지 URL 에디터에 삽입
+             */
             editor.insertEmbed(range.index, 'image', url)
-            // URL 삽입 후 커서를 이미지 뒷 칸으로 이동
-            editor.setSelection(range.index + 1)
+            /**
+             * URL 삽입 후 커서를 이미지 뒷 칸으로 이동
+             */
+            editor.setSelection(range.index + 1, 0)
           })
         })
       } catch (error) {
