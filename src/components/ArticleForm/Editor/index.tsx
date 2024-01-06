a17ac59a67a4789c53f2cfe6498cc64f235f00a1
Seonmi Jung <devkatej927@gmail.com>
2024-01-06 10:23:36 +0900

    refactor : to aviod Promise return, added anonymous function

--- src/components/ArticleForm/Editor/index.tsx
+++ src/components/ArticleForm/Editor/index.tsx
@@ -23,37 +23,39 @@ const Editor = ({ contentHtml, onChangeContent }: Props) => {
     input.setAttribute('type', 'file')
     input.setAttribute('accept', 'image/*')
     input.click()
-    input.addEventListener('change', async () => {
-      if (!quillRef?.current || input.files === null) return
+    input.addEventListener('change', () => {
+      ;(async () => {
+        if (!quillRef?.current || input.files === null) return
 
-      const editor = quillRef.current.getEditor()
-      const file = input.files[0]
-      const range = editor.getSelection(true)
+        const editor = quillRef.current.getEditor()
+        const file = input.files[0]
+        const range = editor.getSelection(true)
 
-      try {
-        /**
-         * 파일명을 "image/Date.now()"로 저장
-         */
-        const storageRef = ref(storage, `image/${Date.now()}`)
+        try {
+          /**
+           * 파일명을 "image/Date.now()"로 저장
+           */
+          const storageRef = ref(storage, `image/${Date.now()}`)
 
-        /**
-         * Firebase Method : uploadBytes, getDownloadURL
-         */
+          /**
+           * Firebase Method : uploadBytes, getDownloadURL
+           */
 
-        const snapShot = await uploadBytes(storageRef, file)
-        const url = await getDownloadURL(snapShot.ref)
+          const snapShot = await uploadBytes(storageRef, file)
+          const url = await getDownloadURL(snapShot.ref)
 
-        /**
-         * 이미지 URL 에디터에 삽입
-         */
-        editor.insertEmbed(range.index, 'image', url)
-        /**
-         * URL 삽입 후 커서를 이미지 뒷 칸으로 이동
-         */
-        editor.setSelection(range.index + 1, 0)
-      } catch (error) {
-        console.log(error)
-      }
+          /**
+           * 이미지 URL 에디터에 삽입
+           */
+          editor.insertEmbed(range.index, 'image', url)
+          /**
+           * URL 삽입 후 커서를 이미지 뒷 칸으로 이동
+           */
+          editor.setSelection(range.index + 1, 0)
+        } catch (error) {
+          console.log(error)
+        }
+      })()
     })
   }
 
