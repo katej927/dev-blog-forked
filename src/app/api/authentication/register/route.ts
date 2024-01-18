1288c3fb4c90b39db99a094425ebda8f2dc43ae7
Seonmi Jung <devkatej927@gmail.com>
2024-01-18 10:55:31 +0900

    feat : add bcrypt to hash password, add logic that create user account

--- src/app/api/authentication/register/route.ts
+++ src/app/api/authentication/register/route.ts
@@ -1,9 +1,17 @@
 import { NextRequest, NextResponse } from 'next/server'
+import bcrypt from 'bcryptjs'
+
+import { connectMongoDB } from '@/libs/mongodb'
+import User from '@/models/user'
 
 export async function POST(request: NextRequest) {
   try {
-    // TODO: 여기 처리.
     const { name, email, password } = await request.json()
+
+    const hashedPassword = await bcrypt.hash(password, 10)
+
+    await connectMongoDB()
+    await User.create({ name, email, password: hashedPassword })
 
     return NextResponse.json({ message: 'User registered.' }, { status: 201 })
   } catch (error) {
