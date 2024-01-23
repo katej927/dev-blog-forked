1da981f9f6b56a3dc4c38b959f49610bdaf61ce8
Seonmi Jung <devkatej927@gmail.com>
2024-01-23 19:41:12 +0900

    fix : test for github action's error fix

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -1,4 +1,1 @@
-export const API_URL =
-  process.env.NODE_ENV === 'development'
-    ? process.env.NEXT_PUBLIC_DEV_URL
-    : process.env.NEXT_PUBLIC_PROD_URL
+export const API_URL = process.env.NEXTAUTH_URL
