31fcc7dbc17646559ad91c21926cda20e1003e89
Seonmi Jung <devkatej927@gmail.com>
2024-01-27 22:36:28 +0900

    fix : add logic that search in title & content

--- src/app/api/articles/route.ts
+++ src/app/api/articles/route.ts
@@ -36,7 +36,34 @@ export const GET = async (request: NextRequest) => {
   }
 
   const articles = searchTerm
-    ? await Article.find(searchCondition).populate('content')
+    ? await Article.aggregate([
+        {
+          $lookup: {
+            from: 'articlecontents',
+            localField: 'content',
+            foreignField: '_id',
+            as: 'content',
+          },
+        },
+        {
+          $unwind: {
+            path: '$content',
+            preserveNullAndEmptyArrays: true,
+          },
+        },
+        {
+          $match: searchCondition,
+        },
+        {
+          $project: {
+            title: 1,
+            content: { _id: 1 },
+            createdAt: 1,
+            updatedAt: 1,
+            __v: 1,
+          },
+        },
+      ])
     : await Article.find()
 
   return NextResponse.json({ articles })
