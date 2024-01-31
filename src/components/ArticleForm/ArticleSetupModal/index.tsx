3f1bfa610dc4f4aeafbdcbee3989925a8b577296
Seonmi Jung <devkatej927@gmail.com>
2024-01-31 19:47:48 +0900

    feat : adding UI of 'ArticleSetupModal'

--- src/components/ArticleForm/ArticleSetupModal/index.tsx
+++ src/components/ArticleForm/ArticleSetupModal/index.tsx
@@ -1,7 +1,29 @@
+import { useState } from 'react'
 import styles from './index.module.css'
 
 function ArticleSetupModal() {
-  return <div className={styles.container}>카테고리 설정</div>
+  const [showCategoryList, setShowCategoryList] = useState<boolean>(false)
+
+  return (
+    <div className={styles.container}>
+      <h3>카테고리 설정</h3>
+
+      {showCategoryList ? (
+        <section>
+          <input type="text" placeholder="새로운 카테고리 이름을 입력하세요." />
+          <div>카테고리 리스트</div>
+          <button type="button" onClick={() => setShowCategoryList(false)}>
+            취소
+          </button>
+          <button type="button">선택하기</button>
+        </section>
+      ) : (
+        <button type="button" onClick={() => setShowCategoryList(true)}>
+          카테고리에 추가하기
+        </button>
+      )}
+    </div>
+  )
 }
 
 export default ArticleSetupModal
