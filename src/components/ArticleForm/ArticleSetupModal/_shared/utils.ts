044e74c30d2d55ae8f21e8fb5c9daca55b5383f7
Seonmi Jung <devkatej927@gmail.com>
2024-02-05 21:14:06 +0900

    feat : add CREATE category logic - detail : seperated 'loadCategories' method, changed name ('categoryList' -> 'categories')

---
+++ src/components/ArticleForm/ArticleSetupModal/_shared/utils.ts
@@ -0,0 +1,16 @@
+import { CategoryInterface, getCategories } from '@/apis/categories'
+
+export const loadCategories = async () => {
+  try {
+    const res = await getCategories()
+
+    if (!res.ok) {
+      throw new Error('Failed to get categories.')
+    }
+
+    const categories: CategoryInterface[] = await res.json()
+    return categories
+  } catch (error) {
+    console.log(error)
+  }
+}
--- src/components/ArticleForm/ArticleSetupModal/index.tsx
+++ src/components/ArticleForm/ArticleSetupModal/index.tsx
@@ -1,36 +1,80 @@
-import { useState } from 'react'
+import { useState, ChangeEvent } from 'react'
 import styles from './index.module.css'
-import { getCategories, CategoryInterface } from '@/apis/categories'
+import { CategoryInterface, createCategory } from '@/apis/categories'
+import { loadCategories } from './_shared'
 
 function ArticleSetupModal() {
-  const [showCategoryList, setShowCategoryList] = useState<boolean>(false)
-  const [categoryList, setCategoryList] = useState<CategoryInterface[]>([])
+  const [showCategories, setShowCategories] = useState<boolean>(false)
+  const [categories, setCategories] = useState<CategoryInterface[]>([])
+
+  const [newCategoryName, setNewCategoryName] = useState<string>()
+
+  const handleChangeNewCategoryName = ({
+    target: { value },
+  }: ChangeEvent<HTMLInputElement>) => setNewCategoryName(value)
+
+  const handleClickCreatingNewCategoryButton = async () => {
+    if (!newCategoryName) return alert('카테고리 이름을 입력하세요.')
+
+    try {
+      const res = await createCategory({
+        categoryName: newCategoryName,
+      })
+
+      if (!res.ok) {
+        throw new Error('Failed to create a category.')
+      }
+
+      const categories = await loadCategories()
+      setCategories(categories ?? [])
+
+      setNewCategoryName('')
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
   const handleClickAddingCategoryButton = async () => {
-    const res = await getCategories()
-    const categories = await res.json()
-    setCategoryList(categories)
+    const categories = await loadCategories()
+    setCategories(categories ?? [])
 
-    setShowCategoryList(true)
+    setShowCategories(true)
   }
 
   return (
     <div className={styles.container}>
       <h3>카테고리 설정</h3>
 
-      {showCategoryList ? (
+      {showCategories ? (
         <section>
-          <input type="text" placeholder="새로운 카테고리 이름을 입력하세요." />
+          <input
+            value={newCategoryName}
+            type="text"
+            placeholder="새로운 카테고리 이름을 입력하세요."
+            onChange={handleChangeNewCategoryName}
+          />
+          <div>
+            <button type="button" onClick={() => setNewCategoryName('')}>
+              취소
+            </button>
+            <button
+              type="button"
+              onClick={handleClickCreatingNewCategoryButton}
+            >
+              카테고리 추가
+            </button>
+          </div>
+
           <ul>
-            {categoryList.length ? (
-              categoryList.map(({ _id, categoryName }) => (
+            {categories.length ? (
+              categories.map(({ _id, categoryName }) => (
                 <li key={_id}>{categoryName}</li>
               ))
             ) : (
               <div>카테고리가 없습니다.</div>
             )}
           </ul>
-          <button type="button" onClick={() => setShowCategoryList(false)}>
+          <button type="button" onClick={() => setShowCategories(false)}>
             취소
           </button>
           <button type="button">선택하기</button>
