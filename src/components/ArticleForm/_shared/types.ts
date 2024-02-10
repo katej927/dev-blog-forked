428d5a79a8b05afd41223afb14bd22c3f4732aaf
Seonmi Jung <devkatej927@gmail.com>
2024-02-10 21:25:45 +0900

    fix : removed useless type ('ArticleTitleContentType')

--- src/components/ArticleForm/_shared/types.ts
+++ src/components/ArticleForm/_shared/types.ts
@@ -1,10 +1,5 @@
 import { ArticleDetailInterface } from '@/apis/articles'
 
-export type ArticleTitleContentType = Pick<
-  ArticleDetailInterface,
-  'title' | 'content'
->
-
 export interface ArticleFormProps extends ArticleDetailInterface {
   onSubmit: (article: ArticleDetailInterface) => Promise<void>
 }
