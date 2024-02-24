e362c6bebfaa2ebf2ba7bc2d8c34328c61e7936e
Seonmi Jung <devkatej927@gmail.com>
2024-02-24 19:51:51 +0900

    fix : fixed build errors - detail : 1. any -> unknown 2. add title in iframe

--- src/app/analytics.tsx
+++ src/app/analytics.tsx
@@ -25,6 +25,7 @@ const Analytics = () => {
       {/* <!-- Google Tag Manager (noscript) --> */}
       <noscript>
         <iframe
+          title="Google Tag Manager"
           src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
           height="0"
           width="0"
