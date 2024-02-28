62266554a1dc621e0fa27c5ccd749006191f3c22
Seonmi Jung <devkatej927@gmail.com>
2024-02-28 20:25:17 +0900

    feat : add keywords of LAYOUT_METADATA

--- src/constants/metaDatas.ts
+++ src/constants/metaDatas.ts
@@ -1,5 +1,6 @@
 export const LAYOUT_METADATA = {
   title: 'kate-devlog',
   description: 'A dev blog written with the aim of reviewing',
+  keywords: ['kate', 'devlog', 'frontend'],
   verification: { google: process.env.NEXT_PUBLIC_GOOGLE_SITE_VERIFICATION },
 }
