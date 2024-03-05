38397ea03c4d5c7795aa31f081d554abb516c67e
Seonmi Jung <devkatej927@gmail.com>
2024-03-05 20:55:47 +0900

    design : add 'cx' into Home page

--- src/containers/Home/index.tsx
+++ src/containers/Home/index.tsx
@@ -1,8 +1,12 @@
 import Link from 'next/link'
+import classNames from 'classnames/bind'
 
 import { GetArticlesResponseInterface } from '@/apis/articles'
 
 import Search from './Search'
+import styles from './index.module.css'
+
+const cx = classNames.bind(styles)
 
 interface Props {
   articles: GetArticlesResponseInterface['articles'] | undefined
@@ -13,13 +17,15 @@ const Home = ({ articles }: Props) => {
     <main>
       <Search />
       {articles ? (
-        articles.map(({ title, _id }) => (
-          <Link key={_id} href={`/article/${_id}`}>
-            <div style={{ border: '1px solid black' }}>
-              <div>{title}</div>
-            </div>
-          </Link>
-        ))
+        <ul className={cx('articlesWrapper')}>
+          {articles.map(({ title, _id }) => (
+            <li style={{ border: '1px solid black' }}>
+              <Link key={_id} href={`/article/${_id}`}>
+                {title}
+              </Link>
+            </li>
+          ))}
+        </ul>
       ) : (
         <div>작성된 글이 없습니다.</div>
       )}
