03e73909a5bf701a8ffd7dd6fe3bebb5eec0c4ac
Seonmi Jung <devkatej927@gmail.com>
2024-03-06 19:26:23 +0900

    design : applied date format into each articles

--- src/containers/Home/index.tsx
+++ src/containers/Home/index.tsx
@@ -5,6 +5,7 @@ import { GetArticlesResponseInterface } from '@/apis/articles'
 
 import Search from './Search'
 import styles from './index.module.css'
+import dayjs from 'dayjs'
 
 const cx = classNames.bind(styles)
 
@@ -22,7 +23,9 @@ const Home = ({ articles }: Props) => {
             <li key={_id} style={{ border: '1px solid black' }}>
               <Link href={`/article/${_id}`}>
                 <h2>{title}</h2>
-                <time dateTime={createdAt}>{createdAt}</time>
+                <time dateTime={createdAt}>
+                  {dayjs(createdAt).format('YYYY.MM.DD')}
+                </time>
               </Link>
             </li>
           ))}
