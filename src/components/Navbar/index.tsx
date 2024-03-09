ca5be109d6b21667cfd4cf6f2aa2bafa23f088d1
Seonmi Jung <devkatej927@gmail.com>
2024-03-09 10:31:31 +0900

    design : navigation - details : design write part

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -2,6 +2,11 @@
 
 import Link from 'next/link'
 import { signOut } from 'next-auth/react'
+import { TfiWrite } from 'react-icons/tfi'
+import classNames from 'classnames/bind'
+import styles from './index.module.css'
+
+const cx = classNames.bind(styles)
 
 import useIsLogin from '@/hooks/useIsLogin'
 
@@ -9,14 +14,19 @@ const Navbar = () => {
   const { isLoading, isLoggedin, session } = useIsLogin()
 
   return (
-    <nav style={{ display: 'flex' }}>
+    <nav className={cx('wrapper')}>
       <Link href={'/'}>Home</Link>
       <Link href={'/category'}>Category</Link>
       {!isLoading && (
         <>
           {isLoggedin ? (
-            <div>
-              <Link href={'/article/write'}>Write</Link>
+            <div className={cx('userContentsWrapper')}>
+              <Link href={'/article/write'}>
+                <div className={cx('writeWrapper')}>
+                  <TfiWrite />
+                  <div className={cx('writeText')}>Write</div>
+                </div>
+              </Link>
               <div>{session?.user?.name}</div>
               <button onClick={() => signOut()}>Log Out</button>
             </div>
